eabda1e814b5e9b9969d93702789827dc0004dc9
roma <pouwedeouroma@gmail.com>
2026-01-15 21:21:18 +0000

    Config PostgreSQL et gitignore backend

--- backend/lamessin_backend/lamessin_backend/settings.py
+++ backend/lamessin_backend/lamessin_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from . import credentials
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'lamessin_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': credentials.DB_NAME,
+        'USER': credentials.DB_USER,
+        'PASSWORD': credentials.DB_PASSWORD,
+        'HOST': credentials.DB_HOST,
+        'PORT': credentials.DB_PORT,
     }
 }
 
