2f40c68b2b5ce4047b41224ec451abcec06c28de
roma <pouwedeouroma@gmail.com>
2026-02-07 23:39:11 +0000

    configuration backend: views, et urls pour le login et l'inscription

--- backend/lamessin_backend/lamessin_app/urls.py
+++ backend/lamessin_backend/lamessin_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 
-from .views import *
-
-from django.urls import path
+from .import views
 
 urlpatterns = [
-      path('test/', test_connexion, name='test-api'),
+# Route pour le Connexion
+      path('login/', views.Login.as_view(), name='token_obtain_pair'),
+
+# Route pour l'Inscription
+      path('register/', views.Inscription.as_view(), name='register'),
+
+# Route pour le Profil é
+      path('profile/', views.UserProfil.as_view(), name='user_profile'),
 ]
 
